e3ea44cfb08e306e9051f7464a277a04e8f8e97f
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-08 09:34:21 +0200

    Implement prefix/suffix

--- module/index.js
+++ module/index.js
@@ -1,4 +1,5 @@
 const assign = require('object-assign');
+const drop = require('1-liners/drop');
 
 let error;
 
@@ -8,7 +9,19 @@ const tinyError = (args) => {
     'Wrong argument. We expected `{Object} args` or `{String} message`.'
   );
 
-  if (args.hasOwnProperty('message')) return assign(
+  const hasMessage = args.hasOwnProperty('message');
+
+  if (hasMessage && args.hasOwnProperty('prefix')) {
+    args.message = args.prefix + args.message;
+    args = drop('prefix', args);
+  }
+
+  if (hasMessage && args.hasOwnProperty('suffix')) {
+    args.message = args.message + args.suffix;
+    args = drop('suffix', args);
+  }
+
+  if (hasMessage) return assign(
     Object.create(args.hasOwnProperty('prototype') ?
       args.prototype :
       ((args.type && args.type.prototype) || Error.prototype)
