b70310ac660139a5d8cf19418c78205d9efc7497
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-08 09:30:40 +0200

    Return an error object

--- module/index.js
+++ module/index.js
@@ -1,9 +1,19 @@
+const assign = require('object-assign');
+
 let error;
 
 const tinyError = (args) => {
   if (typeof args === 'string') args = {message: args};
   else if (typeof args !== 'object') throw error(
     'Wrong argument. We expected `{Object} args` or `{String} message`.'
+  );
+
+  if (args.hasOwnProperty('message')) return assign(
+    Object.create(args.hasOwnProperty('prototype') ?
+      args.prototype :
+      ((args.type && args.type.prototype) || Error.prototype)
+    ),
+    args
   );
 };
 
