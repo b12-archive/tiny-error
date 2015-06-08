51212b3187864bd2e361d0652a10ac52b7d5ee3f
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-08 09:43:53 +0200

    Implement currying

--- module/index.js
+++ module/index.js
@@ -3,12 +3,16 @@ const drop = require('1-liners/drop');
 
 let error;
 
-const tinyError = (args) => {
-  if (typeof args === 'string') args = {message: args};
-  else if (typeof args !== 'object') throw error(
+const normalizeArgs = (args) => {
+  if (typeof args === 'object') return args;
+  if (typeof args === 'string') return {message: args};
+  throw error(
     'Wrong argument. We expected `{Object} args` or `{String} message`.'
   );
+};
 
+const tinyError = (args) => {
+  args = normalizeArgs(args);
   const hasMessage = args.hasOwnProperty('message');
 
   if (hasMessage && args.hasOwnProperty('prefix')) {
@@ -21,12 +25,16 @@ const tinyError = (args) => {
     args = drop('suffix', args);
   }
 
-  if (hasMessage) return assign(
-    Object.create(args.hasOwnProperty('prototype') ?
-      args.prototype :
-      ((args.type && args.type.prototype) || Error.prototype)
-    ),
-    args
+  return (hasMessage ?
+    assign(
+      Object.create(args.hasOwnProperty('prototype') ?
+        args.prototype :
+        ((args.type && args.type.prototype) || Error.prototype)
+      ),
+      args
+    ) :
+
+    (extraArgs) => tinyError(assign({}, args, normalizeArgs(extraArgs)))
   );
 };
 
