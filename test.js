ee0a14106de1496ea094bda0b3d8ea54cb778790
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-08 09:11:25 +0200

    Specify error object

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 import tinyError from './module/index';
 
 const test = require('tape-catch');
+const assign = require('object-assign');
 
 test('The API is in good shape', (is) => {
   is.equal(
@@ -12,13 +13,13 @@ test('The API is in good shape', (is) => {
   is.equal(
     typeof tinyError('message'),
     'object',
-    'returning an error object when given a string,'
+    '…returning an error object when given a string,'
   );
 
   is.equal(
     typeof tinyError({message: 'message'}),
     'object',
-    'or an args object with the key `message`.'
+    '…or an args object with the key `message`.'
   );
 
   const curriedError = tinyError({any: 'other', keys: 'and values'});
@@ -32,20 +33,103 @@ test('The API is in good shape', (is) => {
   is.equal(
     typeof curriedError({message: 'and', other: 'stuff'}),
     'object',
-    'which again returns an error object when given a message,'
+    '…which again returns an error object,'
   );
 
   is.equal(
     typeof curriedError({}),
     'function',
-    'or curries the function further.'
+    '…or curries the function further.'
   );
 
-  is.equal(
+  is.deepEqual(
     tinyError({three: 3})({two: 2})({one: 1})('go!'),
     tinyError({three: 3, two: 2, one: 1, message: 'go!'}),
     'Returns the same results whether curried or not'
   );
 
+  is.end();
+});
+
+test('Creates a lightweight error object', (is) => {
+  is.equal(
+    tinyError('message').__proto__,
+    Error.prototype,
+    'Returns an object based on `Error.prototype`,'
+  );
+
+  is.equal(
+    tinyError({
+      type: TypeError,
+      message: 'message',
+    }).__proto__,
+    TypeError.prototype,
+    '…except when `type` is specified,'
+  );
+
+  const customProto = {};
+  is.equal(
+    tinyError({
+      prototype: customProto,
+      message: 'message',
+    }).__proto__,
+    customProto,
+    '…or a custom `prototype` is given.'
+  );
+
+  is.equal(
+    tinyError({
+      type: TypeError,
+      prototype: customProto,
+      message: 'message',
+    }).__proto__,
+    customProto,
+    '`prototype` takes precedence over `type`.'
+  );
+
+  is.equal(
+    tinyError({message: 'My message'}).message,
+    'My message',
+    'Copies over the property `message`,'
+  );
+
+  is.equal(
+    tinyError({
+      prefix: '[my label] ',
+      message: 'My message',
+    }).message,
+    '[my label] My message',
+    '…extending it when `prefix`'
+  );
+
+  is.equal(
+    tinyError({
+      prefix: '[my label] ',
+      message: 'My message',
+      suffix: '. Good one, ain’ it?',
+    }).message,
+    '[my label] My message. Good one, ain’ it?',
+    '…or `suffix` is given.'
+  );
+
+  is.deepEqual(
+    tinyError({
+      customKey: 'anything',
+      prefix: '[prefix] ',
+      message: 'My message',
+      name: 'CustomError'
+    }),
+    assign(Object.create(Error.prototype), {
+      customKey: 'anything',
+      message: '[prefix] My message',
+      name: 'CustomError'
+    }),
+    'Copies over all other args.'
+  );
+
+  is.end();
+});
+
+test('Fails gracefully', (is) => {
   is.end();
 });
